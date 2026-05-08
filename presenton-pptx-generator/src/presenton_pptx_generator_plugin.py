2dc1f4fad240f3ff25083b8367046b034e5ee9b8
wuyyyyyou <2680574248@qq.com>
2026-05-08 09:50:30 +0800

    Bump plugin versions to 1.2.0

--- presenton-pptx-generator/src/presenton_pptx_generator_plugin.py
+++ presenton-pptx-generator/src/presenton_pptx_generator_plugin.py
@@ -9,7 +9,7 @@ TOOL_NAME = "generatePptx"
 MANIFEST = {
     "name": "tool-lightvoss_5433-ppt-gener-dc7ftcep",
     "display_name": "ppt-gener",
-    "version": "1.1.0",
+    "version": "1.2.0",
     "description": "Anna Executa plugin for generating .pptx files from PptxPresentationModel JSON files.",
     "author": "Anna Developer",
     "tools": [
