76ebbf866970538326ad64be9511f15b34484acf
wuyyyyyou <2680574248@qq.com>
2026-06-12 04:01:12 +0800

    update tool_id and version

--- ppt-app/src/api/toolManifests.generated.ts
+++ ppt-app/src/api/toolManifests.generated.ts
@@ -2,13 +2,13 @@
 // Do not edit it by hand.
 
 export const PPT_ENGINE_TOOL = {
-  id: "tool-lightvoss_5433-ppt-engine-6443rj2a",
-  version: "4.0.2",
+  id: "tool-youming_5703-ppt-engine-fmkv9ru7",
+  version: "3.1.6",
   displayName: "ppt-engine",
 } as const;
 
 export const PPT_GENER_TOOL = {
-  id: "tool-lightvoss_5433-ppt-gener-dc7ftcep",
+  id: "tool-youming_5703-ppt-gener-ahzv8re6",
   version: "2.0.2",
   displayName: "ppt-gener",
 } as const;
